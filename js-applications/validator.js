e97e5987997901bd1dfec23df3311d0049518725
artemioJjJ <artemiyromanoff@gmail.com>
2022-09-19 16:50:06 +0300

    Fixed lots of problems. Need to fix php script time

--- js-applications/validator.js
+++ js-applications/validator.js
@@ -1,64 +1,68 @@
-const form = document.forms.inputValues;
-const yInput = form.yValue;
-const rInput = form.rValue;
+        const form = document.forms.inputValues;
+        const yInput = form.yValue;
+        const rInput = form.rValue;
+        const tbody = document.querySelector(".action-area__right-subarea__history-table > table > tbody")
 
-const submitButton = form.submitButton;
+        const submitButton = form.submitButton;
 
-const COLOR__WRONG_RED =  "#B22222";
-const Y_MIN_VALUE = -5;
-const Y_MAX_VALUE = 3;
-const R_MAX_VALUE = 4;
-const R_MIN_VALUE = 1;
+        const COLOR__WRONG_RED =  "#B22222";
+        const Y_MIN_VALUE = -5;
+        const Y_MAX_VALUE = 3;
+        const R_MAX_VALUE = 4;
+        const R_MIN_VALUE = 1;
 
-submitButton.onclick = function(e) {
-    e.preventDefault();
-    let xValue = form.elements.xValue.value;
-    let yValue = form.yValue.value;
-    let rValue = form.rValue.value;
+        submitButton.onclick = function(e) {
+            e.preventDefault();
+            let xValue = form.elements.xValue.value;
+            let yValue = form.yValue.value;
+            let rValue = form.rValue.value;
 
-    yValue = yValue.replace(",", ".");
-    rValue = rValue.replace(",", ".");
+            yValue = yValue.replace(",", ".");
+            rValue = rValue.replace(",", ".");
 
-    let y = parseFloat(yValue);
-    let r = parseFloat(rValue);
+            let y = parseFloat(yValue);
+            let r = parseFloat(rValue);
 
-    function changeState(elem) {
-        elem.style.background = COLOR__WRONG_RED;
-    }
+            function changeState(elem) {
+                elem.style.background = COLOR__WRONG_RED;
+            }
 
-    if ((isNaN(y) || isNaN(r)) || (y < Y_MIN_VALUE || y > Y_MAX_VALUE) || (r < R_MIN_VALUE || r > R_MAX_VALUE)) {
-        if (isNaN(y) || y < Y_MIN_VALUE || y > Y_MAX_VALUE) {
-            changeState(yInput);
-        }
-        if (isNaN(r) || r < R_MIN_VALUE || r > R_MAX_VALUE) {
-            changeState(rInput);
-        }
-    } else if (((!isNaN(y)) && !isNaN(r)) && (y >= Y_MIN_VALUE && y <= Y_MAX_VALUE) && (r >= R_MIN_VALUE || r <= R_MAX_VALUE)) {
-        function request() {
-            let body = new FormData();
-            body.append("x", xValue);
-            body.append("y", y.toString());
-            body.append("r", r.toString());
-            return fetch("http://localhost:5001/server.php", {
-                method: 'POST',
-                body: body,
-                headers: {
+            if ((isNaN(y) || isNaN(r)) || (y < Y_MIN_VALUE || y > Y_MAX_VALUE) || (r < R_MIN_VALUE || r > R_MAX_VALUE)) {
+                if (isNaN(y) || y < Y_MIN_VALUE || y > Y_MAX_VALUE) {
+                    changeState(yInput);
                 }
-            });
-
-            //TODO method json() instead of text()
-        }
-        request()
-            .then(response => {
-                if (response.ok) {
-                    return response.text();
+                if (isNaN(r) || r < R_MIN_VALUE || r > R_MAX_VALUE) {
+                    changeState(rInput);
                 }
-            })
-            .then(data => console.log(data));
-    }
-}
+            } else if (((!isNaN(y)) && !isNaN(r)) && (y >= Y_MIN_VALUE && y <= Y_MAX_VALUE) && (r >= R_MIN_VALUE || r <= R_MAX_VALUE)) {
+                function request() {
+                    let body = new FormData();
+                    body.append("x", xValue);
+                    body.append("y", y.toString());
+                    body.append("r", r.toString());
+                    return fetch("server.php", {
+                        method: 'POST',
+                        body: body,
+                        headers: {
+                        }
+                    });
 
-form.oninput = function () {
-    yInput.style.background = "white";
-    rInput.style.background = "white";
-}
+                    //TODO method json() instead of text()
+                }
+                request()
+                    .then(response => {
+                        if (response.ok) {
+                            return response.text();
+                        }
+                    })
+                    .then(data => {
+                        console.log(data);
+                        tbody.insertAdjacentHTML("beforeend", data);
+                    });
+            }
+        }
+
+        form.oninput = function () {
+            yInput.style.background = "white";
+            rInput.style.background = "white";
+        }
